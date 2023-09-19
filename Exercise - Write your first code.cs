801a9537f7c367a546ff4d5db704fb0f137a1ce6
richard <finickydesert@users.noreply.github.com>
2023-09-19 16:45:36 +0000

    Update and rename Exercise - Write your first code.cs to Store and retrieve data using literal and variable values in C#.cs

--- Exercise - Write your first code.cs
+++
@@ -1,9 +0,0 @@
-// basic hello world unit 2
-
-//Console.WriteLine("Hello World!");
-
-//had to comment out previous line because it told me. 
-
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
