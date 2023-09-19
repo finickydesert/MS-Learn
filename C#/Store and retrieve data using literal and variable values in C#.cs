1c0e64498ac859ccee325fd72db4831bf50b5c8a
finickydesert <rherist@gmail.com>
2023-09-19 20:17:56 +0000

    updates

--- C#/Store and retrieve data using literal and variable values in C#.cs
+++ C#/Store and retrieve data using literal and variable values in C#.cs
@@ -1,1 +1,18 @@
 // notes here
+string firstName;
+firstName = "bob";
+Console.WriteLine(firstName);
+// then 
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+//initialize as soon as possible to get it right; you can even initalize it right on one line like
+string firstName = "Bob";
+Console.WriteLine(firstName);
