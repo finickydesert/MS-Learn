5b2c426f6a7aa510290beae0b3097f5294737704
richard <finickydesert@users.noreply.github.com>
2023-09-19 16:23:35 +0000

    Update Exercise - Write your first code.cs
    corrected it

--- Exercise - Write your first code.cs
+++ Exercise - Write your first code.cs
@@ -1,2 +1,9 @@
 // basic hello world unit 2
-Console.WriteLine("Hello World!");
+
+//Console.WriteLine("Hello World!");
+
+//had to comment out previous line because it told me. 
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
