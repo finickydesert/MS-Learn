5e0c2d9fa5fb9a0412d77508597b1d5ffbeb2d49
richard <finickydesert@users.noreply.github.com>
2023-09-19 16:34:11 +0000

    Write your first C# code.cs
    first part

---
+++ 900 XP Write your first C# code.cs
@@ -0,0 +1,26 @@
+// basic hello world unit 2
+
+//Console.WriteLine("Hello World!");
+
+//had to comment out previous line because it told me. 
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+
+// Learn how it works 
+
+Console.WriteLine("1.Check your knowledge");
+
+Console.WriteLine("What is the difference between Console.Write and Console.WriteLine?");
+
+// Console.Write prints the output on a new line.
+
+// Console.WriteLine prints the output on a new line.
+
+Console.WriteLine("Console.WriteLine appends a new line after the output.");
+
+// Write your first C# code Complete the challenge solution unit 4
+Console.WriteLine("Challenge: Write code in the .NET Editor to display two messages");
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
